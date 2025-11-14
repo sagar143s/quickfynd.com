452e72163ec72a059ee4ffdfb0d6064459c9832b
Rohith Sagar <rohithsagar14325@gmail.com>
2025-11-14 11:47:55 +0400

    Update Facebook Meta Pixel ID

--- app/layout.jsx
+++ app/layout.jsx
@@ -39,7 +39,7 @@ export default function RootLayout({ children }) {
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '816279901299449');
+              fbq('init', '1509420293656480');
               fbq('track', 'PageView');
             `,
           }}
@@ -49,7 +49,7 @@ export default function RootLayout({ children }) {
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=816279901299449&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1509420293656480&ev=PageView&noscript=1"
           />
         </noscript>
         {/* End Meta Pixel Code */}
